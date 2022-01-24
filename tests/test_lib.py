e8a186668fa8b8c6abd78dc25d9dd763b3ff8cb6
stephanyvargas <yostephy@hotmail.com>
2022-01-24 14:03:28 +0900

    test for city_weather

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,7 +1,8 @@
 # tests/test_lib.py
-from awesome_commands.lib import change_toarray
+from awesome_commands.lib import change_toarray, city_weather
 import numpy
 
 def test_change_tolist():
     assert type(change_toarray([48.8, 2.3, 48.2, 2.3])) == numpy.ndarray
     assert type(change_toarray('abc')) == str
+    assert city_weather() == None
